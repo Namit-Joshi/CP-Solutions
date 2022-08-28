70ba5dbed215657722398a9ae7924004f0978d92
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:22:24 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1472C | Long Jumps/103246504.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ int n;
+ cin>>n;
+ 
+ int a[n+1];
+ 
+ int c[n+1];
+ 
+ for(int i=0;i<=n;i++)
+ {
+  c[i] = 0;
+ }
+ 
+ for(int i=1;i<=n;i++)
+ {
+  cin>>a[i];
+ }
+ 
+ ll ans  = 0;
+ 
+ for(int i=1;i<=n;i++)
+ {
+  ll s = 0;
+ 
+  if(c[i]==0)
+  {
+   for(int j=i;j<=n;)
+   {
+    if(c[j]==1)
+     break;
+    
+    c[j] = 1;
+    s+=a[j];
+    j+=a[j];
+   }
+   ans = max(ans,s);
+  } 
+ }
+ 
+ cout<<ans<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
