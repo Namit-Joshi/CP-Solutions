8daac7bfd8bf9a0dc6cb567a62bb928ec1a6177b
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:21:19 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1703D | Double Strings/169546772.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ int n;
+ cin>>n;
+ 
+ string s[n];
+ map<string,int> mp;
+ 
+ for(int i=0;i<n;i++)
+ {
+  cin>>s[i];
+  mp[s[i]] = 1;
+ }
+ 
+ for(int i=0;i<n;i++)
+ {
+  int fg = 0;
+ 
+  for(int j=1;j<s[i].size();j++)
+  {
+   string pre = s[i].substr(0,j);
+   string suf = s[i].substr(j,s[i].size()-j);
+ 
+   if(mp[pre] && mp[suf])
+   { 
+    fg = 1;
+   }
+  }
+  cout<<fg;
+ }
+ cout<<endl;
+ 
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
