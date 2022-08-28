687b677400650d40ea839824072fb600164f35bc
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:37:05 +0530

    Committing files

---
+++ CodeForces/GNU C++17/110A | Nearly Lucky Number/71567844.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+long long int n;
+cin>>n;
+string s;
+s=to_string(n);
+int count=0;
+for(int i=0;i<s.length();i++)
+{
+    if(s[i]=='4' || s[i]=='7')
+    count++;
+}
+if(count==4||count==7)
+cout<<"YES";
+else
+cout<<"NO";
+return 0;
+}
