f2d287d7b241644755985472d62b67f1b1b1039b
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:23:55 +0530

    Committing files

---
+++ CodeForces/GNU C++17/287A | IQ Test/96801582.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ char a[4][4];
+ int c = 0;
+ 
+ for(int i=0;i<4;i++)
+ {
+  for(int j=0;j<4;j++)
+  {
+   cin>>a[i][j];
+  }
+ }
+ 
+ for(int i=0;i<3;i++)
+ {
+  for(int j=0;j<3;j++)
+  {
+   c = 0;
+   
+   if(a[i][j]=='.')
+    c++;
+   if(a[i+1][j]=='.')
+    c++;
+   if(a[i][j+1]=='.')
+    c++;
+   if(a[i+1][j+1]=='.')
+    c++;
+ 
+   // cout<<c<<" ";
+ 
+   if(c!=2)
+   {
+    cout<<"YES"<<endl;
+    return;
+   }
+  }
+ }
+ 
+ cout<<"NO"<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+//cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
