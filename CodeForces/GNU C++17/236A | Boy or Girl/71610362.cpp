2fe213a70c9f945a9db99ca14b8ed4995e20b13d
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:36:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17/236A | Boy or Girl/71610362.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+string s;
+cin>>s;
+set<int>a;
+for(int i=0;i<s.length();i++)
+{
+    a.insert(s[i]);
+}
+if(a.size()%2==0)
+    cout<<"CHAT WITH HER!";
+else
+    cout<<"IGNORE HIM!";
+return 0;
+}
