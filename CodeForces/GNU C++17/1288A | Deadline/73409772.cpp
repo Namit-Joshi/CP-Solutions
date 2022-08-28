1e42450890e29b3a24a4512c600e2932ce0f554c
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:35:13 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1288A | Deadline/73409772.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+void test()
+{
+ long long int n,d,f;
+ cin>>n>>d;
+ 
+ for (int i = 0; i <= sqrt(d); ++i)
+ {
+  f=i+ceil(d/(i+1.0));
+  // cout<<f<<endl;
+  if(f<=n)
+  {
+   cout<<"YES"<<endl;
+   return;
+  } 
+ }
+ cout<<"NO"<<endl;
+} 
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
