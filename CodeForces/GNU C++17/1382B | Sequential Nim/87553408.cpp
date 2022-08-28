b84dcb827c5b4c0fb99331de74e84ca4b8f8b8a9
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:27:24 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1382B | Sequential Nim/87553408.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ int n,f=1,c=0;
+ cin>>n;
+ 
+ int a[n];
+ 
+ for(int i=0;i<n;i++)
+ {
+  cin>>a[i];
+ }
+ 
+ if(n==1 || a[0]!=1)
+ {
+  cout<<"First"<<endl;
+  return;
+ }
+ 
+ int i=0;
+ 
+ while(a[i]==1 && i!=n-1)
+ {
+  c++;
+  i++;
+ }
+ 
+ if(c%2==0)
+ {
+  cout<<"First"<<endl;
+ }
+ else
+ {
+  cout<<"Second"<<endl;
+ }
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
