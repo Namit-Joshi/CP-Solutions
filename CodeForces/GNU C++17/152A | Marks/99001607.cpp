ba7989f0f4ac28085b2b130393143040da47829f
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:23:14 +0530

    Committing files

---
+++ CodeForces/GNU C++17/152A | Marks/99001607.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+    int n,m;
+    cin>>n>>m;
+ 
+    char a[n][m];
+ 
+    vector<char> v;
+ 
+    int ans = 0;
+ 
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+ 
+    for(int i=0;i<m;i++)
+    {
+        char b = '0';
+ 
+        for(int j=0;j<n;j++)
+        {
+            b = max(b,a[j][i]);
+        }
+ 
+        v.pb(b);
+    }
+ 
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i][j] == v[j])
+            {
+                ans++;
+                break;
+            }
+        }
+    }
+ 
+    cout<<ans<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+// cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+    test();
+}
+return 0;
+}
