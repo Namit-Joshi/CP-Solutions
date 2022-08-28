ad4310691e9fbbd557737fcf16d50ae19cea1ba8
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:29:17 +0530

    Committing files

---
+++ CodeForces/GNU C++17/352A | Jeff and Digits/85103459.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ int n,five=0,zero=0;
+ cin>>n;
+ 
+ for(int i=0;i<n;i++)
+ {
+  int a;
+  cin>>a;
+ 
+  if(a==5)
+   five++;
+  else
+   zero++;
+ }
+ 
+ if(zero==0)
+ {
+  cout<<-1<<endl;
+  return;
+ }
+ 
+ if(five<9)
+ {
+  cout<<0<<endl;
+  return;
+ }
+ 
+ five=five/9;
+ 
+ cout<<string(five*9,'5');
+ cout<<string(zero,'0');
+ cout<<endl;
+ 
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+//cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
