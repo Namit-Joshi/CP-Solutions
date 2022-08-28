8a5db2340430641e8fc30294f48dbbe3314a2c88
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:22:36 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1466C | Canine poetry/102818711.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define MOD 1000000007
+#define PI 3.14159265359
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define dbg(x) cout << #x << " = " << x << '\n'
+#define dbg2(x,y) cout<<#x<<" = "<<x<<" & "<<#y<<" = "<<y<<endl;
+ 
+void test()
+{
+ string s;
+ cin>>s;
+ 
+ int n = s.size();
+ 
+ if(n==2)
+ {
+  if(s[0]==s[1])
+  {
+   cout<<1<<endl;
+   return;
+  }
+  else
+  {
+   cout<<0<<endl;
+   return;
+  }
+  // return;
+ }
+ 
+ for(int i=0;i<n-2;i++)
+ {
+  if(s[i+2]==s[i+1] && s[i+1]==s[i])
+  {
+   s[i+2] = '_';
+   s[i+1] = '_';
+  }
+  else if(s[i+2]==s[i+1])
+  {
+   s[i+2] = '_';
+  }
+  else if(s[i+2]==s[i])
+  {
+   s[i+2] = '_';
+  }
+  else if(s[i+1]==s[i])
+  {
+   s[i+1] = '_';
+  }
+ }
+ 
+ int ans = 0;
+ 
+ for(int i=0;i<n;i++)
+ {
+  if(s[i]=='_')
+   ans++;
+ }
+ 
+ // cout<<s<<endl;
+ 
+ cout<<ans<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
