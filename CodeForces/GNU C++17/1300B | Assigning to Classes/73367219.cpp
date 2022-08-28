2a0b9e87263f6c6cb73ba97a2a8dd78cb4b53e4f
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:35:33 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1300B | Assigning to Classes/73367219.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+void test()
+{
+ int n;
+ cin>>n;
+  int m=2*n;
+  long long int a[m];
+ for (int i = 0; i < m; ++i)
+ {
+  cin>>a[i];
+ }
+ sort(a,a+m);
+ 
+ cout<<a[n]-a[n-1]<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
