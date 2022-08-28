abd722e3d865c5019c0e48cb591c2b348fb8c087
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:37:30 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1173A | Nauuo and Votes/71474975.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int x,y,z;
+cin>>x>>y>>z;
+if(x>y+z)
+cout<<"+";
+else if(y>x+z)
+cout<<"-";
+else if(z==0 && x==y)
+cout<<"0";
+else cout<<"?";
+return 0;
+}
