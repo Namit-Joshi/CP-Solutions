9772741644f2450ec454e852225ee27b43772e8a
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:35:19 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1291A | Even But Not Even/73390017.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef double db;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef complex<double> com;
+ 
+#define endl '\n'
+#define eb emplace_back
+#define pb push_back
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define unique_sort(x) (sort(x.begin(), x.end()), x.resize(distance(x.begin(), unique(x.begin(), x.end()))))
+#define fast_io() ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+ 
+void test()
+{
+ int n,co=0;
+ cin>>n;
+ string str;
+ cin>>str;
+ for (int i = 0; i < n; ++i)
+ {
+  if(str[i]=='1' || str[i]=='3' || str[i]=='5' || str[i]=='7' || str[i]=='9')
+   co++;
+ 
+  if(co==2)
+  {
+   n=i;
+   break;
+  }
+ }
+ if(co!=2)
+ {
+  cout<<-1<<endl;
+  return;
+ }
+ for (int i = 0; i <= n; ++i)
+ {
+  cout<<str[i];
+ }
+ cout<<endl;
+}
+ 
+int main()
+{   
+fast_io();
+cout << fixed << setprecision(15);
+int t = 1;
+ 
+cin>>t;
+ 
+for(int i = 0; i < t; i++)
+{
+ test();
+}
+return 0;
+}
