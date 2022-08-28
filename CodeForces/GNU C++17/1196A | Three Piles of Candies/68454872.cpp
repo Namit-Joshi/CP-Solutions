8c48048c878f1e56f62f2c34a0f31e2024e02291
Namit Joshi <42213693+Namit-Joshi@users.noreply.github.com>
2022-08-28 14:39:08 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1196A | Three Piles of Candies/68454872.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+long long int a,b,c;
+int i,q;
+cin>>q;
+for(i=0;i<q;i++)
+{
+    cin>>a>>b>>c;
+    cout<<(a+b+c)/2<<endl;
+}
+return 0;
+}
